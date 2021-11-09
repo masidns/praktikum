09df3c6c4e4b99a1d4c75d0c4f0ae781dcc0d3c0
masidns <masidns@gmail.com>
2021-11-10 00:52:15 +0900

    testing

---
+++ index.php
@@ -0,0 +1,181 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" type="text/css" href="bootstrap-3/css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="bootstrap-3/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <title>My Porfolio!</title>
+</head>
+
+<body>
+  <!-- <h1>Hello, world!</h1> -->
+  <!-- jumbotron -->
+  <div class="jumbotron jumbotron-fluid text-center">
+    <div class="container">
+      <img img src="img/denim.jpeg" class="img-thumbnail" id="img-circle">
+      <h1>Deni Malik</h1>
+      <p>Desainer | Programer Malas Ngoding | Gamers</p>
+    </div>
+  </div>
+  <!-- jumbotron -->
+
+  <section class="about" id="about">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <h2 class="text-center">About</h2>
+          <hr>
+        </div>
+      </div>
+
+      <div class="row text-justify">
+        <!-- <div class="col-sm-1"></div> -->
+        <div class="col-sm-5 col-sm-offset-1">
+          Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical
+          Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at
+          Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a
+          Lorem
+          Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable
+          source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The
+          Extremes
+          of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very
+          popular
+          during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line
+          in
+          section 1.10.32.
+        </div>
+        <div class="col-sm-5">
+          Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical
+          Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at
+          Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a
+          Lorem
+          Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable
+          source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The
+          Extremes
+          of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very
+          popular
+          during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line
+          in
+          section 1.10.32.
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="portfolio" id="portfolio">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <h2 class="text-center">Portfolio</h2>
+          <hr>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-sm-4">
+          <a href="" class="thumbnail">
+            <img src="img/1.jpeg" class=""></img>
+          </a>
+        </div>
+        <div class="col-sm-4">
+          <a href="" class="thumbnail">
+            <img src="img/2.jpg" class=""></img>
+          </a>
+        </div>
+        <div class="col-sm-4">
+          <a href="" class="thumbnail">
+            <img src="img/3.jpg" class=""></img>
+          </a>
+        </div>
+        <div class="col-sm-4">
+          <a href="" class="thumbnail">
+            <img src="img/1.jpeg" class=""></img>
+          </a>
+        </div>
+        <div class="col-sm-4">
+          <a href="" class="thumbnail">
+            <img src="img/2.jpg" class=""></img>
+          </a>
+        </div>
+        <div class="col-sm-4">
+          <a href="" class="thumbnail">
+            <img src="img/3.jpg" class=""></img>
+          </a>
+        </div>
+      </div>
+
+    </div>
+  </section>
+
+  <section class="contact" id="contact">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <h2 class="text-center">Contact</h2>
+          <hr>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-sm-6 col-sm-offset-3">
+          <form>
+            <div class="form-group">
+              <label for="nama">Nama </label>
+              <input type="text" id="nama" class="form-control" placeholder="Masukan Nama">
+            </div>
+            <div class="form-group">
+              <label for="email">Email </label>
+              <input type="email" id="email" class="form-control" placeholder="Masukan email">
+            </div>
+            <div class="form-group">
+              <label for="telp">Telepon </label>
+              <input type="tel" id="telp" class="form-control" placeholder="Masukan No. telepon">
+            </div>
+            <select name="" id="" class="form-control">
+              <option value="">-- Pilih Kategori --</option>
+              <option value="">Desaigner UI/UX</option>
+              <option value="">Perogramer</option>
+              <option value="">Gamers</option>
+            </select>
+            <div class="form-group">
+              <label for="telp">Pesan </label>
+              <textarea name="text" id="pesan" cols="" rows="3" class="form-control" placeholder="Masukan Pesan"></textarea>
+            </div>
+            <div class="form-group text-right">
+              <button class="btn btn-primary float-right">Kirim Pesan</button>
+            </div>
+          </form>
+        </div>
+      </div>
+
+    </div>
+  </section>
+
+
+  <script src="bootstrap-3/js/bootstrap.min.js"></script>
+  <script src="bootstrap-3/js/bootstrap.js"></script>
+  <!-- Optional JavaScript; choose one of the two! -->
+
+  <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+  <!-- <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+      integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous">
+    </script> -->
+
+  <!-- Option 2: Separate Popper and Bootstrap JS -->
+  <!--
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
